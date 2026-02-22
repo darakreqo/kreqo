3a884fe8310c50dd568968ea0700d2ab31785bc4
darakreqo <dara.kreqo@gmail.com>
2026-02-21 20:34:59 -0500

    Move ExternMethod to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,23 @@
 pub mod database;
 pub mod errors;
 pub mod users;
+
+pub trait ExternMethod
+where
+    Self: Sized,
+{
+    fn apply<F>(self, method: F) -> Self
+    where
+        F: Fn(Self) -> Self,
+    {
+        method(self)
+    }
+    fn apply_with<F, O>(self, method: F, options: O) -> Self
+    where
+        F: Fn(Self, O) -> Self,
+    {
+        method(self, options)
+    }
+}
+
+impl<T> ExternMethod for T {}
